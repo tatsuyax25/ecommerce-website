9a66127f09f8813d57e92f8edecd4109b165d766
Miguel Urena <urena_miguel82@outlook.com>
2022-11-04 13:34:24 -0700

    Add SingleProduct pt. 1

--- src/pages/product/[title].tsx
+++ src/pages/product/[title].tsx
@@ -5,3 +5,23 @@ import NextImage from "next/image";
 import { useRouter } from "next/router";
 import Navbar from "../../components/Navbar";
 import Skelton from "../../components/Skelton";
+
+const stripePromiseclientSide = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+const SingleProduct = () => {
+    const router = useRouter();
+
+    const getSingleProduct = async () => {
+        try {
+            const title = router?.query?.title;
+
+            const respJSON = await fetch(`/api/products/${title}`);
+            const resp = await respJSON.json();
+            return resp;
+        } catch (error) {
+            throw error;
+        }
+    };
+};
