029cefc0f709037d8d43e54f0cfea253d445d79c
Miguel Urena <urena_miguel82@outlook.com>
2022-11-07 14:18:50 -0800

    HTML pt. 1

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -3,3 +3,15 @@ import { useRouter } from "next/router";
 import { HiCheckCircle } from "react-icons/hi";
 import Navbar from "../components/Navbar";
 
+const ThankYou = () => {
+    const router = useRouter();
+    return(
+        <div>
+            <Head>
+                <title>Thank You</title>
+                <meta name="description" content="All Products" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    )
+}
